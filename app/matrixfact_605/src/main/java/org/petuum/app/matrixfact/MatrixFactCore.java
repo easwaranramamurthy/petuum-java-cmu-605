d7b9a601fa9eb336dc3d0f7edad9f5539ab9a5d4
Easwaran Ramamurthy <eramamur@andrew.cmu.edu>
2015-04-27 19:33:48 -0400

    added my own code

--- app/matrixfact_605/src/main/java/org/petuum/app/matrixfact/MatrixFactCore.java
+++ app/matrixfact_605/src/main/java/org/petuum/app/matrixfact/MatrixFactCore.java
@@ -17,29 +17,93 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 
 public class MatrixFactCore {
-    private static final Logger logger =
-        LoggerFactory.getLogger(MatrixFactCore.class);
+  private static final Logger logger = LoggerFactory
+      .getLogger(MatrixFactCore.class);
 
-    // Perform a single SGD on a rating and update LTable and RTable
-    // accordingly.
-    public static void sgdOneRating(Rating r, double learningRate,
-            DoubleTable LTable, DoubleTable RTable, int K, double lambda) {
-        // TODO
+  // Perform a single SGD on a rating and update LTable and RTable
+  // accordingly.
+  public static void sgdOneRating(Rating r, double learningRate,
+      DoubleTable LTable, DoubleTable RTable, int K, double lambda) {
+    // TODO
+    DoubleRow Li = LTable.get(r.userId);
+    DoubleRow Rj = RTable.get(r.prodId);
+
+    double Eij = r.rating - computeDotProduct(Li, Rj, K);
+    double ni = Li.get(K);
+    double nj = Rj.get(K);
+
+    DoubleRowUpdate lUpdate = new DenseDoubleRowUpdate(K);
+    DoubleRowUpdate rUpdate = new DenseDoubleRowUpdate(K);
+    for (int i = 0; i < K; i++) {
+      lUpdate.setUpdate(i,
+          2 * learningRate * (Eij * Rj.get(i) - lambda * Li.get(i) / ni));
+      rUpdate.setUpdate(i,
+          2 * learningRate * (Eij * Li.get(i) - lambda * Rj.get(i) / nj));
     }
 
-    // Evaluate square loss on entries [elemBegin, elemEnd), and L2-loss on of
-    // row [LRowBegin, LRowEnd) of LTable,  [RRowBegin, RRowEnd) of Rtable.
-    // Note the interval does not include LRowEnd and RRowEnd. Record the loss to
-    // lossRecorder.
-    public static void evaluateLoss(ArrayList<Rating> ratings, int ithEval,
-            int elemBegin, int elemEnd, DoubleTable LTable,
-            DoubleTable RTable, int LRowBegin, int LRowEnd, int RRowBegin,
-            int RRowEnd, LossRecorder lossRecorder, int K, double lambda) {
-        // TODO
-        double sqLoss = 0;
-        double totalLoss = 0;
-        lossRecorder.incLoss(ithEval, "SquareLoss", sqLoss);
-        lossRecorder.incLoss(ithEval, "FullLoss", totalLoss);
-        lossRecorder.incLoss(ithEval, "NumSamples", elemEnd - elemBegin);
+    LTable.batchInc(r.userId, lUpdate);
+    RTable.batchInc(r.prodId, rUpdate);
+
+  }
+
+  private static double computeDotProduct(DoubleRow li, DoubleRow rj, int end) {
+    double dotProduct = 0;
+    for (int i = 0; i < end; i++) {
+      dotProduct += li.get(i) * rj.get(i);
     }
+    return dotProduct;
+  }
+
+  // Evaluate square loss on entries [elemBegin, elemEnd), and L2-loss on of
+  // row [LRowBegin, LRowEnd) of LTable, [RRowBegin, RRowEnd) of Rtable.
+  // Note the interval does not include LRowEnd and RRowEnd. Record the loss to
+  // lossRecorder.
+  public static void evaluateLoss(ArrayList<Rating> ratings, int ithEval,
+      int elemBegin, int elemEnd, DoubleTable LTable, DoubleTable RTable,
+      int LRowBegin, int LRowEnd, int RRowBegin, int RRowEnd,
+      LossRecorder lossRecorder, int K, double lambda) {
+//    double sqLoss = 0;
+//    double totalLoss = 0;
+
+    for (int i = elemBegin; i < elemEnd; i++) {
+      Rating r = ratings.get(i);
+      DoubleRow Li = LTable.get(r.userId);
+      DoubleRow Rj = RTable.get(r.prodId);
+      double sqLossAdd = Math.pow(r.rating - computeDotProduct(Li, Rj, K), 2);
+ 
+      lossRecorder.incLoss(ithEval, "SquareLoss", sqLossAdd);//could be commented
+      lossRecorder.incLoss(ithEval, "FullLoss", sqLossAdd);//could be commented
+
+//      sqLoss += sqLossAdd;
+//      totalLoss += sqLossAdd;
+    }
+
+    double totalLossInc = lambda
+        * (frobNormSquare(LTable, LRowBegin, LRowEnd, K) + frobNormSquare(
+            RTable, RRowBegin, RRowEnd, K));
+    
+    lossRecorder.incLoss(ithEval, "FullLoss", totalLossInc);//could be commented
+
+//    lossRecorder.incLoss(ithEval, "SquareLoss", sqLoss);
+//    lossRecorder.incLoss(ithEval, "FullLoss", totalLoss);
+    lossRecorder.incLoss(ithEval, "NumSamples", elemEnd - elemBegin);
+  }
+
+  private static double frobNormSquare(DoubleTable table, int rowBegin,
+      int rowEnd, int K) {
+    double result = 0;
+    for (int i = rowBegin; i < rowEnd; i++) {
+      result += frobNormSquare(table.get(i), K);
+    }
+    return result;
+  }
+
+  private static double frobNormSquare(DoubleRow row, int K) {
+    double result = 0;
+    for (int i = 0; i < K; i++) {
+      result += Math.pow(row.get(i), 2);
+    }
+    return result;
+  }
+
 }
